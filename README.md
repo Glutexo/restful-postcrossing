618175b8bf78449838706993ace11bf34cd0afbb
Glutexo <glutexo@me.com>
2014-01-01 18:39:35 +0100

    Finish the first version. Add README.

---
+++ README.md
@@ -0,0 +1,9 @@
+# RESTful Postcrossing #
+
+## What is this script? ##
+
+This Greasemonkey userscript simplifies the process of registering a postcard received through the [Postcrossing service](http://www.postcrossing.com/). Normally you have to go to the postcard registering page, find the country code in the drop-down list, enter the number and then proceed to filling in the comment for the sender. With this userscript, the process is simplified: You can enter the postcard code directly as a part of the URL. For example, you type http://www.postcrossing.com/CC-123456 to register a postcard with ID CC-123456. Normally, Postcrossing just shows you a 404 page, but with the userscript installed, you are redirected to the registering page with the country code and number filled in. You just have to write your message for the sender of the card. It’s similar to the way, how the coords.info service works for directly accesing entities at [Geocaching.com website](http://www.geocaching.com). Of course you have to be signed in with your Postcrossing account for this userscript to work.
+
+## What is Postcrossing? ##
+
+[Postcrossing](http://www.postcrossing.com/) is a web project for people who like to exchange postcards by real (physical) mail. After signing up, you can request an address of a random person in the world, whom you then have to send a postcard to. For every postcard sent, you receive one in return. The service tries its best for you to have contact with most countries of the world. It’s also a good way, how to find penpals.
--- restful-postcrossing.user.js
+++ restful-postcrossing.user.js
@@ -3,6 +3,47 @@
 // @namespace http://www.hanafuda.cz/
 // @description Adds a RESTful way to register your Postcrossing cards.
 // @include http://www.postcrossing.com/* 
+// @grant none
 // ==/UserScript==
 
-console.log(window.location.href);
+(function() {
+    var restfulUrlRegExp = /^http:\/\/www\.postcrossing\.com\/([A-Z]+)-?(\d+)$/i;
+    var receiveUrlRegExp = /^http:\/\/www\.postcrossing\.com\/receive\?receive\[country_code\]=([A-Z]+)&receive\[number\]=(\d+)$/i;
+    var upperCaseHref = window.location.href.toUpperCase();
+    
+    // The URL is RESTful: www.postcrossing.com/CC-123456
+    var restfulUrlMatch = upperCaseHref.match(restfulUrlRegExp);
+    if(restfulUrlMatch) {
+        var country_code = restfulUrlMatch[1];
+        var number = restfulUrlMatch[2];
+    
+        var redirectHref = 'http://www.postcrossing.com/receive?';
+        redirectHref += 'receive[country_code]=' + country_code + '&';
+        redirectHref += 'receive[number]=' + number;
+        window.location.href = redirectHref;
+    }
+    
+    // Already redirected to the receive page.
+    var receiveUrlMatch = upperCaseHref.match(receiveUrlRegExp);
+    if(receiveUrlMatch) {
+        var country_code = receiveUrlMatch[1].toUpperCase();
+        var number = receiveUrlMatch[2];
+        
+        // Prefill the country code and number.
+        var country_code_input = document.getElementById('receive_country_code');
+        var country_code_options = country_code_input.options;
+        var country_code_options_length = country_code_options.length;
+        for(var i = 0; i < country_code_options_length; i++) {
+            // Find the right country code among the options.
+            if(country_code_options[i].value !== country_code) {
+                continue;
+            }
+            
+            country_code_input.value = country_code;
+            break;
+        }
+        
+        var number_input = document.getElementById('receive_number');
+        number_input.value = number;
+    }
+})();
