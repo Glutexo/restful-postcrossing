e46bc0738312e938ddd961115f0b4f9e8173954e
Glutexo <glutexo@me.com>
2014-01-01 12:08:11 +0100

    initial commit

---
+++ restful-postcrossing.user.js
@@ -0,0 +1,8 @@
+// ==UserScript== 
+// @name RESTful Postcrossing
+// @namespace http://www.hanafuda.cz/
+// @description Adds a RESTful way to register your Postcrossing cards.
+// @include http://www.postcrossing.com/* 
+// ==/UserScript==
+
+console.log(window.location.href);
